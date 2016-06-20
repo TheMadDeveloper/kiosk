ddecc3015ba2abc44895c0fd32fc17984a154e61
TheMadDeveloper <w@keithkerlan.com>
2016-06-19 23:13:45 -0700

    Initial commit of Plex app for LaMetric clock.

---
+++ kiosk.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python
+# encoding=utf-8
+
+import lametric
+
+import urllib, urllib2
+import xml.etree.ElementTree as ET
+import os.path
+import ConfigParser
+
+# Netatmo / LaMetric Proxy
+# Author : Stanislav Likavcan, likavcan@gmail.com
+
+# A simple client which turns LaMetric into Netamo display. This client calls Netatmo API and updates LaMetric display 
+# Easiest way to keep the LaMetric display updated is via cron task:
+# */10 * * * * /home/lametric/updateLaMetric.py
+# Don't forget to create an app within both Netatmo and LaMetric and provide your credentials here:
+
+scriptDir = os.path.dirname(os.path.abspath(__file__))
+
+config = ConfigParser.ConfigParser()
+config.read("%s/config.ini"%(scriptDir))
+
+# Netatmo authentication
+app_id     = config.get('lametric','app_id')
+access_token = config.get('lametric','access_token')
+
+req = urllib2.Request("http://gir.local:32400/status/sessions")
+response = urllib2.urlopen(req).read()
+root = ET.fromstring(response)
+
+is_playing = root.attrib['size'] != '0'
+
+if is_playing:
+    for video in root.findall("Video"):
+        title = video.attrib["title"]
+        year = video.attrib["year"]
+        duration = float(video.attrib["duration"])
+        current = float(video.attrib["viewOffset"])
+
+        print "Now Playing: %s (%s)"%(title,year)
+        print "Currently at %.1f mins of %.1f"%(current/60000, duration/60000)
+
+        print current
+        # Post data to LaMetric
+    	lametric = lametric.Setup()
+    	lametric.addTextFrame('a1944',"On Now")
+    	lametric.addTextFrame('',"%s (%s)"%(title,year))
+    	lametric.addGoalFrame('',0,current/60000, duration/60000," Mins")
+    	lametric.addTextFrame('',"Ends in %i minutes"%((duration-current)/60000))
+    	lametric.push(app_id, access_token)
+        
+        break
---
+++ lametric.py
@@ -0,0 +1,71 @@
+# PythonAPI LaMetric REST data access
+# coding=utf-8
+
+import json
+import urllib2
+import ssl
+
+# Common definitions
+
+#_BASE_URL       = "https://developer.lametric.com/"
+_BASE_URL       = "https://10.0.1.100:4343/"
+_PUSH_URL       = _BASE_URL + "api/v1/dev/widget/update/com.lametric."
+
+class Setup(object):
+
+    def __init__(self):
+        self.data = {}
+        self.data['frames'] = []
+        self.index = 0
+
+    def addTextFrame(self, icon, text):
+        frame = {}
+        frame['index'] = self.index
+        frame['icon']  = icon
+        frame['text']  = text
+        self.data['frames'].append(frame)
+        self.index += 1
+
+    def addGoalFrame(self, icon, start, current, end, unit):
+        frame = {}
+        print current
+        frame['index'] = self.index
+        frame['icon']  = icon
+        frame['goalData'] = {}
+        frame['goalData']['start'] = start
+        frame['goalData']['current'] = current
+        frame['goalData']['end'] = end
+        frame['goalData']['unit'] = unit
+        self.data['frames'].append(frame)
+        self.index += 1
+
+    def addSparklineFrame(self, data):
+        frame = {}
+        frame['index'] = self.index
+        frame['chartData'] = data
+        self.data['frames'].append(frame)
+        self.index += 1
+    
+    def push(self, app_id, access_token):
+        ctx = ssl.create_default_context()
+        ctx.check_hostname = False
+        ctx.verify_mode = ssl.CERT_NONE
+        opener = urllib2.build_opener(urllib2.HTTPSHandler(context=ctx));
+        headers = { 'Accept': 'application/json', 'Cache-Control': 'no-cache', 'X-Access-Token': access_token };
+        request = urllib2.Request(_PUSH_URL + app_id, json.dumps(self.data,ensure_ascii=False), headers);
+
+        print json.dumps(self.data,ensure_ascii=False)
+
+        try:
+            response = opener.open(request);
+        except urllib2.HTTPError as e:
+            print('The server couldn\'t fulfill the request.')
+            print('Error code: ', e.code)
+            print e.read()
+        except urllib2.URLError as e:
+            print('Failed to reach a server.')
+            print('Reason: ', e.reason)
+            print e.read()
+        #else:
+            # everything is fine
+
